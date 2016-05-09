87f626650d7ee315c0b196a254629dddbf298d35
BillyGalbreath <Blake.Galbreath@GMail.com>
2016-05-08 22:12:01 -0500

    Initial commit

---
+++ src/main/java/net/pl3x/bukkit/chatapi/ChatMessageType.java
@@ -0,0 +1,11 @@
+package net.pl3x.bukkit.chatapi;
+
+/**
+ * Represents the position on the screen where a message will appear.
+ */
+@SuppressWarnings("unused")
+public enum ChatMessageType {
+    CHAT,
+    SYSTEM,
+    ACTION_BAR
+}
